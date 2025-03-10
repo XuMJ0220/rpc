88e4f16e666e60c0911f22b1dea572c85f0c61c3
xumingjie <1044011439@qq.com>
2025-03-10 23:54:15 +0800

    异步日志缓冲队列实现

--- src/include/lockqueue.hpp
+++ src/include/lockqueue.hpp
@@ -13,7 +13,25 @@ class LockQueue{
         std::condition_variable condition_variable_;
     public:
         void Push(const T& data);
-        T& Pop();
+        T Pop();
 };
+
+template<typename T>
+void LockQueue<T>::Push(const T& data){
+    std::lock_guard<std::mutex> lock(mutex_);
+    queue_.push(data);
+    condition_variable_.notify_one();
+}
+
+template<typename T>
+T LockQueue<T>::Pop(){
+    std::unique_lock<std::mutex> lock(mutex_);
+    while(queue_.empty()){
+        condition_variable_.wait(lock);
+    }
+    T data = queue_.front();
+    queue_.pop();
+    return data;
+}
 
 #endif 
